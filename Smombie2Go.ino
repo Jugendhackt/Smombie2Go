99ca6ad8f2d46dc5c02f7294048f119a0b2d54a0
Christopher Aicher <aicher83128@gmail.com>
2017-06-14 14:10:33 +0200

    Create Smombie2Go.ino

--- Smombie2Go.ino
+++ Smombie2Go.ino
@@ -1,42 +1,35 @@
-int _pin = 3;
+int sensor = 3;
 int LED = 2;
 
-long dauer=0; // Das Wort dauer ist jetzt eine Variable, unter der die Zeit gespeichert wird, die eine Schallwelle bis zur Reflektion und zurück benötigt. Startwert ist hier 0.
-long entfernung=0; // Das Wort „entfernung“ ist jetzt die variable, unter der die berechnete Entfernung gespeichert wird. Info: Anstelle von „int“ steht hier vor den beiden Variablen „long“. Das hat den Vorteil, dass eine größere Zahl gespeichert werden kann. Nachteil: Die Variable benötigt mehr Platz im Speicher.
+long dauer = 0;
+long entfernung = 0;
 
+bool state;
 
 void setup()
 {
-  Serial.begin (9600); //Serielle kommunikation starten, damit man sich später die Werte am serial monitor ansehen kann.
-  pinMode(_pin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(sensor, OUTPUT);
 }
 
 
 void loop()
 {
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, LOW);
+  pinMode(sensor, OUTPUT);
+  digitalWrite(sensor, LOW);
   delayMicroseconds(2);
-  digitalWrite(_pin, HIGH);
+  digitalWrite(sensor, HIGH);
   delayMicroseconds(5);
-  digitalWrite(_pin,LOW);
-  pinMode(_pin,INPUT);
+  digitalWrite(sensor, LOW);
+  pinMode(sensor, INPUT);
   long duration;
-  duration = pulseIn(_pin,HIGH);
-  long entfernung = duration/29/2;
+  duration = pulseIn(sensor, HIGH);
+  long entfernung = duration / 29 / 2;
 
-  if (!(entfernung >= 500 || entfernung <= 120))
-  {
-  Serial.println(entfernung);
-
-  int t = entfernung - 20;
-  digitalWrite(LED, HIGH); delay(t);
-  digitalWrite(LED, LOW); delay(t);
-  digitalWrite(LED, HIGH); delay(t);
-  digitalWrite(LED, LOW); delay(t);
-  }
+  state = (entfernung < 200)
   
-  delay(1);
-  }
+  digitalWrite(LED, state);
+    
+  delay(2);
 }
 
